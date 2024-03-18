91b863c75b84c56edbf6b958e2ddfa132ee8ad9f
Shayankhan <shayankhan9148@gmail.com>
2024-03-18 11:52:17 +0500

    remaining

---
+++ app.js
@@ -0,0 +1,129 @@
+//                     chapter 14 to 16
+                            //   question 1
+// var studentName = []
+// studentName.push("Muhammad Shayan");
+// console.log(studentName);
+
+//                              question 2
+// var studentName=
+                  //topic is in pending
+
+
+                            //   question 3
+// var studentName=["anas","shayan","walled","areeb"];
+// console.log(studentName)
+
+
+                      //        question 4
+// var num=[1,2,3,4,5,6];
+// console.log(num);
+                         //     question 5
+
+// var boolenValue=[true,false]
+// console.log(boolenValue)
+
+                          //    question 6
+//  var mixedArray=[12,"shayan",false]
+//  console.log(mixedArray)                       
+                          
+                         //    quetion 7
+// var qualification=["SSC","HSC","BCS","BS","MS","MSC","M.PHIL","PHD","AND LAST ONE IS PERSON IS ALSO CONCERED WITH PHYSOCOLOGICAL DOCTOR"]
+// document.write(
+//     `<h1>Qualification</h1>
+//     <ul>
+//    <li>
+//    ${qualification}
+//    </li>
+//    </ul>`
+   
+// );
+// const qualification =[                                                                                                                                                            
+//     "SSC",
+//     "HSC",
+//     "BCS",
+//     "BS",
+//     "MS",
+//     "MSC",
+//     "M.Phil",
+//     "PhD",
+//     "AND LAST ONE IS PERSON IS ALSO CONCERED WITH PHYSOCOLOGICAL DOCTOR",
+// ];
+
+//  // Function to display qualifications
+//  function displayQualifications() {
+//     document.write("<h2>Available Education Qualifications in Pakistan</h2>");
+//     document.write("<ul>");
+//     qualifications.forEach(qualification => {
+//       document.write(`<li>${qualification}</li>`);
+//     });
+//     document.write("</ul>");
+//   }
+
+//   // Call the display function
+//   displayQualifications();
+
+                       //   question 8
+
+// var studentName=["areeb","shayan","ahmed"];
+// var studenMarks=[320,230,480];
+// var totalMarks=500;
+// var studentOne=studenMarks[0]/totalMarks*100;
+// var studentTwo=studenMarks[1]/totalMarks*100;
+// var studentThree=studenMarks[2]/totalMarks*100;
+
+// document.write(
+//     `
+// score of ${studentName[0]} is ${studenMarks[0]} .percentage: ${studentOne}%<br>
+// score of ${studentName[1]} is ${studenMarks[1]} .percentage: ${studentTwo}%<br>    
+// score of ${studentName[2]} is ${studenMarks[2]} .percentage: ${studentThree}%<br>    
+
+//     `
+// )
+
+                            //question 9
+
+
+        // can't be understand
+
+
+                         // question 10
+
+//  var score1 = [320,280,480,120];
+//  score1.sort()
+//   document.write(` Ordered Score of Students: ${score1}
+//   `);
+
+                      //   question 11
+
+
+// var cities = [`Karachi`,"Lahore","Islamabad","Quetta","Peshawar"];
+// var selectedCities = [];
+// selectedCities.push(cities.slice(2,4));
+// document.write(selectedCities)
+
+
+                     // question 12
+
+// var arr = ["This ", " is ", " my ", " cat"];
+// var arrJoin = arr.join("")
+// document.write(`
+//     Array:<br>
+//     ${arr}<br>
+//     <br>
+//     String:<br>
+//     ${arrJoin}
+// `)
+
+                 //question 13
+
+                 //question 14
+
+                 //question 15
+// var manufacturers = ["Apple", "Samsung", "Motorola", "Nokia", "Sony", "Haier"];
+// function generateDropdown() {
+// document.write('<select id="manufacturerDropdown">');
+// for (var i = 0; i < manufacturers.length; i++) {
+// document.write('<option value="' + manufacturers[i] + '">' + manufacturers[i] + '</option>');
+// }
+// document.write('</select>'); }
+// generateDropdown();
